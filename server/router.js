13d82a23bd12e7420eed1e4b92633086512a4bf1
Austin Liu <austin362667@gmail.com>
2021-05-27 05:55:01 +0800

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('server is up and running');
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 module.exports = router;
