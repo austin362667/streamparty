211b223eabee8cf3892a885a15d51ba275c1c268
Austin Liu <austin362667@gmail.com>
2021-05-28 10:47:12 +0800

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-app.get('/*', function (req, res) {
+router.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
