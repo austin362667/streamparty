34df3185d9b8e5a28a6c1ded21a7505582541ef9
Austin Liu <austin362667@gmail.com>
2021-05-28 10:49:47 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const cors = require('cors');
 
 require('dotenv').config()
 
-// app.use(express.static('../client/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // let currVideo = {}; // Video playing in room
 
