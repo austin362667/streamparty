762e193c838b8714b64ef9919e9e3001ea3e9d4e
Austin Liu <austin362667@gmail.com>
2021-05-30 03:45:26 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -25,20 +25,6 @@ function App() {
                 {/* <Route path="/browse" component={Browse} /> */}
             </Switch>
         </Router>
-        <div className="App" style={{ height: "100%" }}>
-          {inCall ? (
-            <VideoCall setInCall={setInCall} />
-          ) : (
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => setInCall(true)}
-            >
-              Join Call
-            </Button>
-          )}
-        </div>
-    </div>
     )
 }
 
