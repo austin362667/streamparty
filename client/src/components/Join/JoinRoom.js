41acc77a6c91072b9f9e020d02f51a5bc6290c68
Austin Liu <austin362667@gmail.com>
2021-06-01 15:41:48 +0800

    Update JoinRoom.js

--- client/src/components/Join/JoinRoom.js
+++ client/src/components/Join/JoinRoom.js
@@ -42,7 +42,7 @@ const JoinRoom = ({ location, history }) => {
                             // icon='sign-in'
                             // labelPosition='right'
                             className='button-join'
-                            onClick={()=>joinRoom}
+                            onClick={()=>{joinRoom();}}
                         />
                         <Spacer height='48px' />
                         <Button
@@ -50,7 +50,7 @@ const JoinRoom = ({ location, history }) => {
                             // icon='sign-in'
                             // labelPosition='right'
                             className='button-join'
-                            onClick={()=>browse}
+                            onClick={()=>{browse();}}
                         />
                     </section>
                 </div >
