10c8aee5a67df0eb9e3d83a9dc7cf1f0422797dc
Austin Liu <austin362667@gmail.com>
2021-06-02 21:25:02 +0800

    Update settings.js

--- client/src/settings.js
+++ client/src/settings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAudioTrack } from "agora-rtc-react";
 
 const appId = "c0fbe3dfdd7c45c8bedac54be5f4fc90";
 const token =
-  "006c0fbe3dfdd7c45c8bedac54be5f4fc90IACfQYlrnYJps/oPNhQiPqRtOHVAiQ6z5ni/mxeersFBRWTNKL8AAAAAEAD7cHweNtW4YAEAAQA21bhg";
+  "006c0fbe3dfdd7c45c8bedac54be5f4fc90IACaEeLecU95bFGKoA98wMaMovMr8B5eRc67Dhf9zBV7tGTNKL8AAAAAEAD7cHweBti4YAEAAQAG2Lhg";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
